f3a6b457ffd2cef1caff7cf7981cbbf1a7e2cd3e
Dave Marquard <dave@marquard.org>
2026-01-20 02:37:13 +0000

    feat: add S3ParsingError type

--- Tests/SwiftS3Tests/ErrorTests.swift
+++ Tests/SwiftS3Tests/ErrorTests.swift
@@ -5,3 +5,9 @@ import Testing
     let error = S3NetworkError(message: "Connection failed", underlyingError: nil)
     #expect(error.message == "Connection failed")
 }
+
+@Test func s3ParsingErrorIncludesResponseBody() async throws {
+    let error = S3ParsingError(message: "Invalid XML", responseBody: "<bad>")
+    #expect(error.message == "Invalid XML")
+    #expect(error.responseBody == "<bad>")
+}
