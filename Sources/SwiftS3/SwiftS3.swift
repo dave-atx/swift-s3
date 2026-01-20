7164b40cdcee3c744293ce5e5fb4387fb5e6be26
Dave Marquard <dave@marquard.org>
2026-01-20 08:07:11 +0000

    feat: finalize public module exports

--- Sources/SwiftS3/SwiftS3.swift
+++ Sources/SwiftS3/SwiftS3.swift
@@ -1,1 +1,7 @@
 // SwiftS3 - S3 Compatible API Client
+// Public API exports
+
+// Re-export all public types for easy importing
+@_exported import struct Foundation.Data
+@_exported import struct Foundation.Date
+@_exported import struct Foundation.URL
