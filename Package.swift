b21941c08b2b5c21df325c467e2aa58c96a62a7b
Dave Marquard <dave@marquard.org>
2026-01-22 16:34:16 +0000

    chore: add swift-argument-parser dependency and ss3 executable target

--- Package.swift
+++ Package.swift
@@ -11,10 +11,15 @@ let package = Package(
         .library(
             name: "SwiftS3",
             targets: ["SwiftS3"]
+        ),
+        .executable(
+            name: "ss3",
+            targets: ["ss3"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
     ],
     targets: [
         .target(
@@ -29,6 +34,20 @@ let package = Package(
         .testTarget(
             name: "SwiftS3Tests",
             dependencies: ["SwiftS3"]
+        ),
+        .executableTarget(
+            name: "ss3",
+            dependencies: [
+                "SwiftS3",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
+        ),
+        .testTarget(
+            name: "ss3Tests",
+            dependencies: ["ss3"]
         )
     ]
 )
